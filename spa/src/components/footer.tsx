216fe33d9bfb053e613e5779bddd391246fd918b
Jonathan Yates <me@jyates.dev>
2026-04-18 10:08:30 -0700

    Fix LinkedIn URL

--- spa/src/components/footer.tsx
+++ spa/src/components/footer.tsx
@@ -40,7 +40,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://www.linkedin.com/in/jyates-dev/"
+            href="https://www.linkedin.com/in/jyatesdotdev/"
           >
             <ArrowIcon />
             <p className="ml-2 h-7 m-0 p-0">linkedin</p>
